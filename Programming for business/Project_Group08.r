93de763dec4e38988db31f65dd51e16f7f4198e2
javi12135 <javi12135+code@gmail.com>
2021-03-12 15:53:15 +0100

    change

--- Programming for business/Project_Group08.r
+++ Programming for business/Project_Group08.r
@@ -3,5 +3,6 @@
 print("Welcome to the Game of the Goose")
 print("How many players play in this game?")
 num_of_players<-0
-while (num_of_players=0) | (num_of_players>=5)
+while (num_of_players=0) | (num_of_players>=5) {
   num_of_players<-scan(,,1)
+}
