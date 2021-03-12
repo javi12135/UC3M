beea383508ba62336ec99992661ad2a6f7589ceb
javi12135 <javi12135+code@gmail.com>
2021-03-12 16:57:50 +0100

    Change

--- Programming for business/Project_Group08.r
+++ Programming for business/Project_Group08.r
@@ -1,8 +1,21 @@
 #Paula Esparza Valdés 100418689
 #Fco. Javier Hernández Ares 100453593
 print("Welcome to the Game of the Goose")
-print("How many players play in this game?")
-num_of_players<-0
-while (num_of_players=0) | (num_of_players>=5) {
-  num_of_players<-scan(,,1)
+num_of_players=100
+while (num_of_players>=5) {
+  print("How many players are in this game? Must be less than 5")
+  num_of_players=scan(,,1)
 }
+Player_1=readline(prompt="Please, insert the name of player 1 ")
+if (num_of_players>=2) {
+  Player_2=readline(prompt="Please, insert the name of player 2 ")
+  if (num_of_players>=3) {
+    Player_3=readline(prompt="Please, insert the name of player 3 ")
+    if (num_of_players==4) {
+      Player_4=readline(prompt="Please, insert the name of player 4 ")
+    }
+  }
+}
+#else {
+#    break
+#}
