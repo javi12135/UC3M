63495fb40dd5b0756b79f9d3ab5baf3f8f653e0d
javi12135 <javi12135+code@gmail.com>
2021-03-12 15:50:32 +0100

    Welcome message

---
+++ Programming for business/Project_Group08.r
@@ -0,0 +1,7 @@
+#Paula Esparza Valdés 100418689
+#Fco. Javier Hernández Ares 100453593
+print("Welcome to the Game of the Goose")
+print("How many players play in this game?")
+num_of_players<-0
+while (num_of_players=0) | (num_of_players>=5)
+  num_of_players<-scan(,,1)
