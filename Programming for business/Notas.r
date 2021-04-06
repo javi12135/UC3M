9424291ebc379a8987159fda122b3a1394851830
javi12135 <javi12135+code@gmail.com>
2021-04-06 12:12:12 +0200

    Class from 2/04/2021

---
+++ Programming for business/Notas.r
@@ -0,0 +1,6 @@
+funcion<-function() {
+  Vector<<-c(1:10)
+  print(Vector)
+  Vector[2]="oca"
+  print(Vector)
+}
--- Programming for business/Project_Group08.r
+++ Programming for business/Project_Group08.r
@@ -1,6 +1,13 @@
 #Paula Esparza Valdés 100418689
 #Fco. Javier Hernández Ares 100453593
-#sample(1:6, 1)
+#Goose spaces: Go to next goose and re-roll. 1,5,9,14,18,23,27,32,36,41,45,50,54,59,63
+#Bridge: Go to the other bridge and re-roll. 6,12
+#Dices: Go to the other dices and re-roll. 26,53
+#Inn: You miss 2 turns. 19
+#Jail: You miss 3 turns. 56
+#Well: You miss 4 turns. 31
+#Mace: Go back to space 30. 42
+#Death: Restart. 58
 
 Menu<-function(){
   print("Welcome to the Game of the Goose")
@@ -11,9 +18,9 @@ MenuChoice<-function(){
   print("What do you want to do?")
   print("1.Choose the name of the players and play")
   print("2.Play the Game of the Goose")
-  print("3.Record the player names and games won")
+  print("3.Record the players' names and games won")
   print("4.Quit the game")
-  choice=scan(,,1)
+  choice<<-scan(,,1)
   if (choice==1){
     Play()
   }
@@ -26,30 +33,39 @@ MenuChoice<-function(){
   if (choice==4){
     EndGame()
   }
+  else {
+    MenuChoice()
+  }
 }
 
 Play<-function(){
+  Tablero<<-c(1:63)
+  Tablero[1,5,9,14,18,23,27,32,36,41,45,50,54,59,63]<<-"Goose"
   Option1()
   Option2()
 }
 
-Option1<-function(){ #Asks number of players and names each player
-  Player_1=readline(prompt="Please, insert the name of player 1 ")
-  YN1="Yes"
+Option1<-function(){ #Since there is only one player, it asks Player 1 for their name
+  Player_1<<-readline(prompt="Please, insert the name of player 1 ")
+  YN1<<-"Yes"
+  Place1<<-0
+  print("Let's play!")
+  print("You can quit the game at any time by pressing 4")
 }
 
 Option2<-function(){ #Goes through each player's turn
-  print("Let's play!")
-  print("You can quit the game at any time by pressing 4")
   if (YN1=="Yes"){
     RollP1()
+  }
+  else if (YN1=="No"){
+    MenuChoice()
   }
 }
 
 RollP1<-function(){ #Asks if P1 wants to roll the dice
-  YN1=readline(prompt="Please, player 1, it's your turn, do you want to roll the dice (Yes/No)")
+  YN1<<-readline(prompt="Please, player 1, it's your turn, do you want to roll the dice (Yes/No)")
   if (YN1=="Yes"){
-    print("lol")
+    Dice1()
   }
   else if (YN1=="No"){
     Option2()
@@ -63,8 +79,19 @@ RollP1<-function(){ #Asks if P1 wants to roll the dice
 }
 
 Dice1<-function(){ #Rolls P1's dice
-  dice<-sample(1:6, 1)
-  Place1=Place1+dice
+  dice<<-sample(1:6, 1)
+  print(dice)
+  Place1<<-Place1+dice
+  print(Place1)
+
+}
+
+
+
+Option3<-function(){ #Records the players' names and games won in .txt file
+  #Save game in .txt file
+  print("Winner will be recorded in .txt file")
+  MenuChoice()
 }
 
 EndGame<-function(){ #Quits the game
