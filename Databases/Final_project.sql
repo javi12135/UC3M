03e93a61f0e667fac1efcaec6c1c5b34de6e3511
javi12135 <javi12135+code@gmail.com>
2021-11-02 19:17:01 +0100

    Added actors, directors, countries and profiles

--- Databases/Final_project.sql
+++ Databases/Final_project.sql
@@ -1,11 +1,35 @@
-create table accounts(
-id VARCHAR2(10),
-firstname VARCHAR2(255) not null,
-lastname VARCHAR2(255) not null,
-email VARCHAR2(255) not null,
-bankaccount CHAR(24) not null,
-countryid CHAR(3) not null,
-gender VARCHAR2(128) CHECK (gender IN ('Male', 'Female', 'Non binary', 'Other', 'I prefer not to say')),
-birthdate DATE,
-primary key(id)
+CREATE TABLE countries(
+  code CHAR(3) PRIMARY KEY,
+  name VARCHAR2(256) NOT NULL
+)
+
+CREATE TABLE directors(
+	code INTEGER PRIMARY KEY,
+  name VARCHAR2(256) NOT NULL
+)
+
+CREATE TABLE actors(
+	id INTEGER PRIMARY KEY,
+  name VARCHAR2(256) NOT NULL
+)
+
+CREATE TABLE accounts(
+	id VARCHAR2(10) PRIMARY KEY,
+	firstname VARCHAR2(256) NOT NULL,
+	lastname VARCHAR2(256) NOT NULL,
+	email VARCHAR2(256) NOT NULL,
+	bankaccount CHAR(24) NOT NULL,
+	countryid CHAR(3) NOT NULL,
+	gender VARCHAR2(128) CHECK (gender IN ('Male', 'Female', 'Non binary', 'Other', 'I prefer not to say')),
+	birthdate DATE,
+
+  FOREIGN KEY (countryid) REFERENCES countries(code)
+)
+
+CREATE TABLE profiles(
+	id INTEGER PRIMARY KEY,
+  name VARCHAR2(256) UNIQUE,
+  accountid VARCHAR2(10) UNIQUE,
+
+  FOREIGN KEY (accountid) REFERENCES accounts(id)
 )
