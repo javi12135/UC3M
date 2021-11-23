e1d986430ddde186b8527ac53ddd1805895bec22
javi12135 <javi12135+code@gmail.com>
2021-11-23 18:48:06 +0100

    Added PROFILES inserts

---
+++ Databases/Final Project/Inserts/profiles.sql
@@ -0,0 +1,7 @@
+set define off;
+insert into profiles values('000001001', 'Javier', 'U000001');
+insert into profiles values('000001002', 'Ines', 'U000001');
+insert into profiles values('000001003', 'Mama', 'U000001');
+insert into profiles values('000002001', 'Paula', 'U000002');
+insert into profiles values('000003001', 'Mayte', 'U000003');
+set define on;
