ad817f3fd161f7bd1b9a9103fbb223b39c06f5fd
javi12135 <javi12135+code@gmail.com>
2021-11-30 12:21:21 +0100

    Added inserts for movieLists

---
+++ Databases/Final Project/Inserts/movieLists.sql
@@ -0,0 +1,8 @@
+set define off;
+insert into MovieLists values('1001001', '1001', 'Animated movies');
+insert into MovieLists values('1001002', '1001', 'Favourite movies');
+insert into MovieLists values('1002001', '1002', 'Love Stories');
+insert into MovieLists values('1003001', '1003', 'Movies to sleep');
+insert into MovieLists values('2001001', '2001', 'Hollywood hits');
+insert into MovieLists values('3001001', '3001', 'Horror movies');
+set define on;
