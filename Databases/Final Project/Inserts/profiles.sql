ad9fad9e0af9c197dbca5109972206e745634ba0
javi12135 <javi12135+code@gmail.com>
2021-11-30 13:46:07 +0100

    Added inserts for viewedMovies

--- Databases/Final Project/Inserts/profiles.sql
+++ Databases/Final Project/Inserts/profiles.sql
@@ -1,7 +1,7 @@
 set define off;
-insert into profiles values('000001001', 'Javier', 'U000001');
-insert into profiles values('000001002', 'Ines', 'U000001');
-insert into profiles values('000001003', 'Mama', 'U000001');
-insert into profiles values('000002001', 'Paula', 'U000002');
-insert into profiles values('000003001', 'Mayte', 'U000003');
+insert into profiles values('1001', 'Javier', 'U000001');
+insert into profiles values('1002', 'Ines', 'U000001');
+insert into profiles values('1003', 'Mama', 'U000001');
+insert into profiles values('2001', 'Paula', 'U000002');
+insert into profiles values('3001', 'Mayte', 'U000003');
 set define on;
