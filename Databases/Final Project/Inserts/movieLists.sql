a8ef15831375e33c07afe169146527bb8db6ddd8
javi12135 <javi12135+code@gmail.com>
2021-11-30 12:21:59 +0100

    Added inserts for movieLists

--- Databases/Final Project/Inserts/movieLists.sql
+++ Databases/Final Project/Inserts/movieLists.sql
@@ -1,17 +1,8 @@
 set define off;
-<<<<<<< HEAD
 insert into MovieLists values('1001001', '1001', 'Animated movies');
 insert into MovieLists values('1001002', '1001', 'Favourite movies');
 insert into MovieLists values('1002001', '1002', 'Love Stories');
 insert into MovieLists values('1003001', '1003', 'Movies to sleep');
 insert into MovieLists values('2001001', '2001', 'Hollywood hits');
 insert into MovieLists values('3001001', '3001', 'Horror movies');
-=======
-insert into profiles values('1001001', '1001', 'Animated movies');
-insert into profiles values('1001002', '1001', 'Favourite movies');
-insert into profiles values('1002001', '1002', 'Love Stories');
-insert into profiles values('1003001', '1003', 'Movies to sleep');
-insert into profiles values('2001001', '2001', 'Hollywood hits');
-insert into profiles values('3001001', '3001', 'Horror movies');
->>>>>>> ff683958e3b4880bfff34e6a71ce5145f376a33b
 set define on;
